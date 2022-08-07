195c1f56f0e2db2212dd3a53e86ab4d4c23c7fdc
william echenim <myron.echenim@gmail.com>
2022-08-06 22:15:49 -0700

    hand api skeleton

---
+++ api/go.mod
@@ -0,0 +1,5 @@
+module github.com/echenim/station/api
+
+go 1.18
+
+require github.com/echenim/station/models v0.0.0-20220806065728-0f2f798907a8
--- api/main.go
+++ api/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	"github.com/echenim/station/api/handlers"
@@ -12,6 +13,7 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
 	product := handlers.NewProductHandler(l)
 
 	sm := http.NewServeMux()
@@ -25,6 +27,22 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 	}
 
-	s.ListenAndServe()
-	s.Shutdown(context.Background())
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	sig := <-c
+	l.Println("shutting down server", sig)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		l.Println("Error shutting down server:", err)
+	}
 }
